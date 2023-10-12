a8a6cf4f673d659810b4524666fe8b8ad0959bf0
bahmetm <quanty.zero@gmail.com>
2023-10-12 19:29:32 +0200

    Init commit

---
+++ src/main/java/com/bahmet/App.java
@@ -0,0 +1,13 @@
+package com.bahmet;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
