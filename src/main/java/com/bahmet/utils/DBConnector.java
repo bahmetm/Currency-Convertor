00d3c42532353de665470a4b21a42d559ae4011f
bahmetm <quanty.zero@gmail.com>
2023-10-15 15:29:23 +0200

    feat: add db connector class

---
+++ src/main/java/com/bahmet/utils/DBConnector.java
@@ -0,0 +1,33 @@
+package com.bahmet.utils;
+
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DBConnector {
+    private static final String url;
+    private static final String user;
+    private static final String password;
+
+    static {
+        try {
+            InputStream input = DBConnector.class.getClassLoader().getResourceAsStream("config.properties");
+            Properties properties = new Properties();
+            properties.load(input);
+            url = properties.getProperty("db.url");
+            user = properties.getProperty("db.user");
+            password = properties.getProperty("db.password");
+
+            Class.forName("org.postgres.Driver");
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("Failed to initialize DBConnector", e);
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url, user, password);
+    }
+}
